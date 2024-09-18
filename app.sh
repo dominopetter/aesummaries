d3582007678ec24c0574db8d5a74f012c5254918
Petter Olsson <petter@dominodatalab.com>
2024-09-18 04:15:34 -0700

    Pushed from Domino: https://presentation.domino-eval.com/workspace/petter/AE_Summaries?executionId=66ea91be052adf30c95545c5

--- app.sh
+++ app.sh
@@ -2,4 +2,5 @@
 # See docs at:
 # https://docs.dominodatalab.com/en/latest/user_guide/e92082/publish-an-r-app-with-shiny/#_publish_an_app
 #R -e 'shiny::runApp("./R/", port=8888, host="0.0.0.0")'
-R -e "options(shiny.port = 8888, shiny.host = '0.0.0.0'); aesummaries::run_app()"
+#R -e "options(shiny.port = 8888, shiny.host = '0.0.0.0'); aesummaries::run_app()"
+R -e "options(shiny.port = 8888, shiny.host = '0.0.0.0'); devtools::load_all(); run_app()"
